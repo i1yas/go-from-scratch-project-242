bf1493046a0ad495bb21fca98a7b3eeda953ad67
i1yas <ilyasers@gmail.com>
2026-07-17 14:22:35 +0300

    Add flag for human-readable format

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -15,6 +15,13 @@ func main() {
 		Name:      "hexlet-path-size",
 		Usage:     "print size of a file or directory",
 		ArgsUsage: "<path>",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "human",
+				Usage:   "print size in human-readable format",
+				Aliases: []string{"H"},
+			},
+		},
 		Arguments: []cli.Argument{
 			&cli.StringArg{
 				Name: "path",
@@ -22,13 +29,15 @@ func main() {
 		},
 		Action: func(ctx context.Context, cmd *cli.Command) error {
 			path := cmd.StringArg("path")
+			isHumanFormat := cmd.Bool("human")
 
 			size, err := code.GetPathSize(path)
+
 			if err != nil {
 				return err
 			}
 
-			fmt.Printf("%dB\t%s\n", size, path)
+			fmt.Printf("%s\t%s\n", formatFileSize(size, isHumanFormat), path)
 			return nil
 		},
 	}
@@ -36,4 +45,38 @@ func main() {
 	if err := cmd.Run(context.Background(), os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+type SizeUnitEntry struct {
+	size    int64
+	postfix string
+}
+
+var sizeUnits = []SizeUnitEntry{
+	{size: int64(1) << 0, postfix: "B"},
+	{size: int64(1) << 10, postfix: "KB"},
+	{size: int64(1) << 20, postfix: "MB"},
+	{size: int64(1) << 30, postfix: "GB"},
+	{size: int64(1) << 40, postfix: "TB"},
+	{size: int64(1) << 50, postfix: "PB"},
+	{size: int64(1) << 60, postfix: "EB"},
+}
+
+func formatFileSize(size int64, isHuman bool) string {
+	if !isHuman {
+		return fmt.Sprintf("%dB", size)
+	}
+
+	unit := sizeUnits[0]
+
+	for _, u := range sizeUnits {
+		if size < u.size {
+			break
+		}
+		unit = u
+	}
+
+	sizeInUnit := float32(size) / float32(unit.size)
+
+	return fmt.Sprintf("%.1f%s", sizeInUnit, unit.postfix)
 }
