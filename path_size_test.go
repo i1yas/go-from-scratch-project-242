6920c915dd099e717b494172b763ab13955e236c
i1yas <ilyasers@gmail.com>
2026-07-21 15:37:05 +0300

    test: Add proper test cases for empty dir

--- path_size_test.go
+++ path_size_test.go
@@ -42,7 +42,6 @@ func TestGetPathSize_Dir(t *testing.T) {
 	}
 
 	cases := []DirTestCase{
-		{dirname: "empty_dir", size: "0B"},
 		{dirname: "dir_with_one_file", size: "6B"},
 		{dirname: "dir_with_nested_dir", size: "6B"},
 		{dirname: "dir_with_hidden_files", size: "6B"},
@@ -72,7 +71,6 @@ func TestGetPathSize_Dir_Recursive(t *testing.T) {
 	}
 
 	cases := []DirTestCase{
-		{dirname: "empty_dir", size: "0B"},
 		{dirname: "dir_with_one_file", size: "6B"},
 		{dirname: "dir_with_nested_dir", size: "12B"},
 		{dirname: "dir_with_hidden_files", size: "6B"},
@@ -87,6 +85,33 @@ func TestGetPathSize_Dir_Recursive(t *testing.T) {
 			}
 
 			size, err := GetPathSize(path, true, false, tCase.includeHidden)
+
+			require.NoError(t, err)
+			require.Equal(t, tCase.size, size)
+		})
+	}
+}
+
+func TestGetPathSize_Empty_Dir(t *testing.T) {
+
+	type EmptyDirTestCase struct {
+		name            string
+		isHumanReadable bool
+		includeHidden   bool
+		size            string
+	}
+
+	cases := []EmptyDirTestCase{
+		{name: "raw format", isHumanReadable: false, includeHidden: false, size: "0B"},
+		{name: "human-readable format", isHumanReadable: true, includeHidden: false, size: "0B"},
+		{name: "raw format and include hidden", isHumanReadable: false, includeHidden: true, size: "0B"},
+	}
+
+	for _, tCase := range cases {
+		t.Run(tCase.name, func(t *testing.T) {
+			tempDir := t.TempDir()
+
+			size, err := GetPathSize(tempDir, true, tCase.isHumanReadable, tCase.includeHidden)
 
 			require.NoError(t, err)
 			require.Equal(t, tCase.size, size)
